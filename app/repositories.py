f039d563bd32f612bd0cb42dbad65a3a87caee12
agungprasastia <agungprasastia@gmail.com>
2025-07-04 11:51:39 +0800

    Penambahan Searching

--- app/repositories.py
+++ app/repositories.py
@@ -1,13 +1,13 @@
 # app/repositories.py
 
 from .models import Karyawan
+from sqlalchemy import or_ # <-- Tambahkan import 'or_'
 
 class KaryawanRepository:
-    def __init__(self, session):
+    def _init_(self, session):
         self.session = session
 
-    def get_by_nip(self, nip):
-        return self.session.query(Karyawan).filter_by(nip=nip).first()
+    # ... (metode get_by_nip, get_by_id, get_all, add, delete tetap sama) ...
 
     def get_by_id(self, karyawan_id):
         return self.session.query(Karyawan).filter_by(id=karyawan_id).first()
@@ -18,9 +18,20 @@ class KaryawanRepository:
     def add(self, karyawan):
         self.session.add(karyawan)
         self.session.commit()
-        self.session.refresh(karyawan) # Refresh untuk mendapatkan ID yang di-generate DB
+        self.session.refresh(karyawan)
         return karyawan
 
     def delete(self, karyawan):
         self.session.delete(karyawan)
         self.session.commit()
+
+    # --- TAMBAHKAN METODE BARU DI BAWAH INI ---
+    def search(self, query_term):
+        """Mencari karyawan berdasarkan nama atau NIP yang cocok sebagian."""
+        search_pattern = f"%{query_term}%"
+        return self.session.query(Karyawan).filter(
+            or_(
+                Karyawan.nama.ilike(search_pattern),
+                Karyawan.nip.ilike(search_pattern)
+            )
+        ).all()
